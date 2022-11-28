adc3eaaba2ee6dfe0392030539233fbe26094f6e
Almaz Salikhov <almaz.salikhov@gmail.com>
2022-11-28 22:28:46 +0200

    Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, Это же МАША!");
+}
+else    
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
