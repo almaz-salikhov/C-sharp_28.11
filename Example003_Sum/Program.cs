996ce56820fa0df3071bdf2e7e6a27b8a21278e7
Almaz Salikhov <almaz.salikhov@gmail.com>
2022-11-28 22:43:16 +0200

    Добавил словесный вывод перед значениями для понятности юзера

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,7 +1,12 @@
 ﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9 (полуинтервал)
+Console.Write("Первое случайное число = ");
 Console.WriteLine(numberA);
+
 int numberB = new Random().Next(1, 10);
+Console.Write("Второе случайное число = ");
 Console.WriteLine(numberB);
+
 int result = (numberA + numberB);
+Console.Write("Сумма чисел = ");
 Console.WriteLine(result);
 
